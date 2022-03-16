e5458ebdcaa4ce3fe92592494df11463d5588ad9
Fernando Gortari <fernando.gortari@gmail.com>
2022-03-15 21:59:57 -0400

    queda por corregir el setTimeout (opcional)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 
 let archivo = '';
 let contenido = '';
+const dia = new Date().getDate();
+const mes = new Date().getMonth();
+const anio = new Date().getFullYear();
+//7. Agrega la fecha actual al comienzo del contenido de cada archivo creado en formato
+//“dd/mm/yyyy”. Considera que si el día o el mes es menor a 10 concatenar un “0” a la
+//izquierda. 
+const fechaCompleta = () => {
+    if(dia < 10 && mes < 10){
+        return `0${dia}/0${mes + 1}/${anio}`;
+    }else if(dia < 10){
+        return `0${dia}/${mes + 1}/${anio}`;
+    }else if(mes < 10){
+        return `${dia}/0${mes + 1}/${anio}`;
+    }else{
+        return `${dia}/${mes + 1}/${anio}`;
+    }
+}
 
 //1. Crear un servidor en Node con el módulo http
 http.createServer((req, res) => {
@@ -20,7 +37,7 @@ http.createServer((req, res) => {
         fs.writeFile(archivo, contenido, () => {
             //6. Devolver un mensaje declarando el éxito o fracaso de lo solicitado en cada consulta
             //recibida.
-            res.write('Archivo creado con exito!');
+            res.write(`(${fechaCompleta()}) - Archivo creado con exito!`);
             res.end();
         })        
     }
@@ -29,7 +46,7 @@ http.createServer((req, res) => {
     if(req.url.includes('/leer')){
         if(fs.existsSync(`./${archivo}`)){
             fs.readFile(archivo, (err, data) => {
-                res.write(data);
+                res.write(`(${fechaCompleta()}) - ${data}`);
                 res.end();
             });
         }else{
@@ -46,7 +63,9 @@ http.createServer((req, res) => {
             fs.rename(`${archivo}`, `${nuevoArchivo}`, (err, data) => {
                 //6. Devolver un mensaje declarando el éxito o fracaso de lo solicitado en cada consulta
                 //recibida.
-                res.write(`Archivo ${archivo} renombrado por ${nuevoArchivo}`);
+                //8. En la ruta para renombrar, devuelve un mensaje de éxito incluyendo el nombre
+                //anterior del archivo y su nuevo nombre de forma dinámica.
+                res.write(`(${fechaCompleta()}) - El Archivo ${archivo} ha sido renombrado por ${nuevoArchivo}`);
                 res.end();
             })
         }else{
@@ -59,19 +78,37 @@ http.createServer((req, res) => {
     //5. Disponibilizar una ruta para eliminar un archivo, cuyo nombre es declarado en los
     //parámetros de la consulta recibida.
     if(req.url.includes('/eliminar')){
-        if(fs.existsSync(`${archivo}`)){
+        fs.existsSync(`${archivo}`)?(
+            res.write(`Tu solicitud para eliminar el archivo ${archivo} se esta procesando
+            `),
+            fs.unlink(`${archivo}`, (err, data) =>{
+                //9. En el mensaje de respuesta de la ruta para eliminar un archivo, devuelve el siguiente
+                //mensaje: “Tu solicitud para eliminar el archivo <nombre_archivo> se está
+                //procesando”, y luego de 3 segundos envía el mensaje de éxito mencionando el
+                //nombre del archivo eliminado.
+                setTimeout(function(){
+                    //6. Devolver un mensaje declarando el éxito o fracaso de lo solicitado en cada consulta
+                    //recibida.
+                    res.write(` - (${fechaCompleta()}) - El archivo ${archivo} ha sido eliminado con exito!`);
+                    res.end();
+                })
+            },3000))
+                //6. Devolver un mensaje declarando el éxito o fracaso de lo solicitado en cada consulta
+                //recibida.
+            :(res.write(`El archivo indicado no existe!`));
+
+        /* if(fs.existsSync(`${archivo}`)){
             fs.unlink(`${archivo}`, (err, data) => {
                 //6. Devolver un mensaje declarando el éxito o fracaso de lo solicitado en cada consulta
                 //recibida.
-                res.write(`Archivo ${archivo} eliminado con éxito`);
+                res.write(`(${fechaCompleta()}) - Archivo ${archivo} eliminado con éxito`);
                 res.end();
             })
         }else{
-                //6. Devolver un mensaje declarando el éxito o fracaso de lo solicitado en cada consulta
-                //recibida.
+                
                 res.write(`El archivo indicado no existe!`);
                 res.end();
-        }        
+        }  */       
     }
 
 }).listen
