de352d6266b51696049da066e2ce983e9c3e4a5a
Fernando Gortari <fernando.gortari@gmail.com>
2022-03-17 22:37:49 -0400

    correcciones realizadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,13 @@ const anio = new Date().getFullYear();
 //Requerimiento 7
 //Generador de fecha
 const fechaCompleta = () => {
-    if(dia < 10 && mes < 10){
+    if (dia < 10 && mes < 10) {
         return `0${dia}/0${mes + 1}/${anio}`;
-    }else if(dia < 10){
+    } else if (dia < 10) {
         return `0${dia}/${mes + 1}/${anio}`;
-    }else if(mes < 10){
+    } else if (mes < 10) {
         return `${dia}/0${mes + 1}/${anio}`;
-    }else{
+    } else {
         return `${dia}/${mes + 1}/${anio}`;
     }
 }
@@ -31,62 +31,69 @@ http.createServer((req, res) => {
     archivo = params.archivo;
     contenido = params.contenido;
     nuevoArchivo = params.nuevoArchivo;
+
+    if (req.url.includes('/')) {
+        fs.readFile('index.html', 'utf-8', (err, html) => {
+            res.end(html);
+        });
+    };
+
     //Requerimiento 2    
-    if(req.url.includes('/crear')){
-        if(archivo == '' || contenido == ''){
+    if (req.url.includes('/crear')) {
+        if (archivo == '' || contenido == '') {
             //Requerimiento 6
             res.write('No se creo el archivo! Debe ingresar información');
             res.end();
-        }else{
+        } else {
             fs.writeFile(archivo, contenido, () => {
                 //Requerimiento 6
                 res.write(`(${fechaCompleta()}) - Archivo creado con exito!`);
                 res.end();
-            })     
+            })
         }
     }
     //Requerimiento 3
-    if(req.url.includes('/leer')){
-        if(fs.existsSync(`./${archivo}`)){
+    if (req.url.includes('/leer')) {
+        if (fs.existsSync(`./${archivo}`)) {
             fs.readFile(archivo, (err, data) => {
                 res.write(`(${fechaCompleta()}) - ${data}`);
                 res.end();
             });
-        }else{
-                //Requerimiento 6
-                res.write('El archivo indicado no existe!');
-                res.end();
-        }     
+        } else {
+            //Requerimiento 6
+            res.write('El archivo indicado no existe!');
+            res.end();
+        }
     }
     //Requerimiento 4
-    if(req.url.includes('/renombrar')){
-        if(fs.existsSync(`./${archivo}`)){
+    if (req.url.includes('/renombrar')) {
+        if (fs.existsSync(`./${archivo}`)) {
             fs.rename(`${archivo}`, `${nuevoArchivo}`, (err, data) => {
                 //Requerimiento 6 || Requerimiento 8
                 res.write(`(${fechaCompleta()}) - El Archivo ${archivo} ha sido renombrado por ${nuevoArchivo}`);
                 res.end();
             })
-        }else{
-                //Requerimiento 6
-                res.write(`El archivo indicado no existe!`);
-                res.end();
+        } else {
+            //Requerimiento 6
+            res.write(`El archivo indicado no existe!`);
+            res.end();
         }
     }
     //Requerimiento 5
-    if(req.url.includes('/eliminar')){
-        fs.existsSync(`${archivo}`)?(            
-            fs.unlink(`${archivo}`, (err, data) =>{
+    if (req.url.includes('/eliminar')) {
+        fs.existsSync(`${archivo}`) ? (
+            fs.unlink(`${archivo}`, (err, data) => {
                 res.write(`Tu solicitud para eliminar el archivo ${archivo} se esta procesando <br>`),
-                //Requerimiento 9
-                setTimeout(function(){
-                    //Requerimiento 6
-                    res.write(`(${fechaCompleta()}) - El archivo ${archivo} ha sido eliminado con exito!`);
-                    return res.end();
-                },3000)
+                    //Requerimiento 9
+                    setTimeout(function () {
+                        //Requerimiento 6
+                        res.write(`(${fechaCompleta()}) - El archivo ${archivo} ha sido eliminado con exito!`);
+                        return res.end();
+                    }, 3000)
             }))
             //Requerimiento 6
-            :(res.write(`El archivo indicado no existe!`),
-            res.end());      
+            : (res.write(`El archivo indicado no existe!`),
+                res.end());
     }
 }).listen
-(8080, () => console.log('Escuchando el puerto 8080'));
+    (8080, () => console.log('Escuchando el puerto 8080'));
